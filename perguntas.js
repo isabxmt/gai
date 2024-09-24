7dcb8b52029553feaaf05198be4fc8857a5c68e4
isabxmt <128045873+isabxmt@users.noreply.github.com>
2024-09-24 07:45:20 -0300

    ,

---
+++ perguntas.js
@@ -0,0 +1,120 @@
+criaCartao(
+    'Química',
+    'Qual é a fórmula química do cloreto de sódio?',
+    'NaCl'
+    )
+    
+    
+    criaCartao(
+    'Química',
+    'O que é uma solução saturada?',
+    'Uma solução onde a quantidade de soluto é a máxima que pode ser dissolvida em uma dada temperatura.')
+    
+    
+    criaCartao(
+    'Química',
+    'Qual é a fórmula química do ácido clorídrico',
+    'HCl')
+    
+    
+    criaCartao(
+    'Química',
+    'Qual é o número atômico do carbono',
+    '6')
+    
+    
+    criaCartao(
+    'Química',
+    'Qual é o pH de uma solução neutra de 25°C',
+    '7')
+    
+    
+    criaCartao(
+    'Química',
+    'Oque é uma reação de oxidação-redução (redox)?',
+    'Uma reação química onde ocorre a transferência de elétrons entre substâncias, resultância em uma mudança dos estados de oxidação')
+    
+    criaCartao(
+    'Química',
+    'Qual é a fórmula molecular do etano?',
+    'C_2H_6')
+    
+    
+    criaCartao(
+    'Química',
+    'Qual é a principal diferença entre um ácido forte e um ácido fracos?',
+    'Um ácido forte se dissocia completamente em solução aquosa, ensaindo um ácido fraco se dissocia parcialmente')
+    
+    criaCartao(
+    'Química',
+    'Qual é a lei de Avogado?',
+    'Volume igual de gases, medidos à mesma temperatura e pressão, contém o mesmo número de moléculas')
+    
+    
+    criaCartao(
+    'Química',
+    'Oque é a constante de equilíbrio de uma reação?',
+    'A constante que expressa a reação entre as concentrações de produtos e dos reagentes em equilíbrio, conforme a equação da reação.')
+    
+    
+    criaCartao(
+    'Química',
+    'Qual é a fórmula do ácido sulfúrico?',
+    'H_2SO_4')
+    
+    
+    criaCartao(
+    'Química',
+    'Qual é o tipo de ligação química predominante na molécula da água?',
+    'Ligação covalente')
+    
+    
+    criaCartao(
+    'Química',
+    'O que é a entalpia de formação padrão?',
+    'A quantidade de energia liberada ou absorvida quando um mol de substância é formado a partir de elementos do seu estados padrão.')
+    
+    
+    criaCartao(
+    'Química',
+    'Qual é a fórmula da glicose?',
+    'C_6H_{12}O_6')
+    
+    
+    
+    criaCartao(
+    'Química',
+    'O que é a eletronegatividade?',
+    'A capacidade de um átomo de atrair elétrons em uma ligação química.')
+    
+    
+    criaCartao(
+    'Química',
+    'O que são isômeros?',
+    'Compostos com a mesma fórmula molécular, mas com estruturas diferentes.')
+    
+    
+    criaCartao(
+    'Química',
+    'Qual é a principal característica dos ácidos de Lewis?',
+    'São substâncias que aceitam pares de elétrons para formarem uma ligação covalente')
+    
+    
+    criaCartao(
+    'Química',
+    'O que é a lei de conservação de massa?',
+    'Em uma reação química, a massa total do reagentes é igual à massa total dos produtos.')
+    
+    
+    criaCartao(
+    'Química',
+    'Qual é o estado físico do brometo de sódio(NaBr) à temperatura ambiente?',
+    'Sólido')
+    
+    
+    
+    criaCartao(
+    'Química',
+    'Qual é o princípio da cromatografia ?',
+    'É uma técnica de separação de misturas com base na diferença de distribuição dos componentes entre uma fase móvel e uma fase estacionária')
+    
